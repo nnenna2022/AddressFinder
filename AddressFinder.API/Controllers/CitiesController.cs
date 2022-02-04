c8820582d030dbde944c0f0830d5d9e5ea1e4e6b
JOY NNENNA ONUH <nnenna.onuh@cyberspace.net.ng>
2022-02-04 11:20:29 +0100

    initial commit

---
+++ AddressFinder.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressFinder.API.Controllers
+{
+    [ApiController]
+    public class CitiesController: ControllerBase
+    {
+        public JsonResult GetAddress()
+        {
+            return new JsonResult(
+               new  List<Object>()
+               {
+                   new{id= 1, Name="New York"},
+                   new {id=2, Name="Canada"}
+               }
+
+                );
+        }
+    }
+}
